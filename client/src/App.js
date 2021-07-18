8be20b49c990ce63517cee52f4ab0517430ca395
Maxim-rrrr <Kewin.rrrr@gmail.com>
2021-07-18 22:23:00 +0300

    +: pages routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./Main";
 import AdminRouter from "./Admin/router"
+
+import Home from './Main/Home'
+import About from './Main/About'
+import Skills from './Main/Skills'
+import Work from './Main/Work'
+import Blog from './Main/Blog'
+import Contact from './Main/Contact'
 
 const App = () => {
   return (
@@ -9,7 +15,24 @@ const App = () => {
         <Route exact path="/admin" component={ AdminRouter } />
         <Route exact path="/admin/*" component={ AdminRouter } />
 
-        <Route path="/*" component={ Main } />
+        <Route exact path="/about" component={ About } />
+        <Route exact path="/about/*" component={ About } />
+
+        <Route exact path="/skills" component={ Skills } />
+        <Route exact path="/skills/*" component={ Skills } />
+
+        <Route exact path="/work" component={ Work } />
+        <Route exact path="/work/*" component={ Work } />
+
+        <Route exact path="/blog" component={ Blog } />
+        <Route exact path="/blog/*" component={ Blog } />
+
+        <Route exact path="/contact" component={ Contact } />
+        <Route exact path="/contact/*" component={ Contact } />
+
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/*" component={ Home } />
+
       </Switch>
     </Router>
   )
