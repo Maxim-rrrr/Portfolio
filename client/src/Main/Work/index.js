2c1b13e1d09e3c9f7c3dea9b01c074399289bb62
Maxim-rrrr <Kewin.rrrr@gmail.com>
2021-12-06 01:00:08 +0300

    +: project mgs

--- client/src/Main/Work/index.js
+++ client/src/Main/Work/index.js
@@ -1,6 +1,7 @@
 import Navigation from "../сomponents/Nav"
 import { Link } from "react-router-dom"
-import img_3d_galley from "./images/3d_gallery.png"
+import img_3d_gallery from "./images/3d_gallery.png"
+import img_mgs from "./images/mgs.png"
 import "./index.sass"
 
 const Work = () => {
@@ -11,24 +12,31 @@ const Work = () => {
                 <div className="work__container">
                     <div className="projects">
                             <Link to='/3d-gallery' className='project'>
-                                <img className="project__img" src={img_3d_galley} alt="" />
+                                <img className="project__img" src={img_3d_gallery} alt="" />
                                 <div className='project__content'>
-                                    <div className="project__title"> Проект 1 </div>
-                                    <div className="project__description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus labore recusandae totam voluptatum, quae alias. </div>
+                                    <div className="project__title"> 3D Галерея </div>
+                                    <div className="project__description"> 
+                                        Галерея с использованием 3D полотен на библиотеке Theer.js
+                                    </div>
                                 </div>
                                 <div className="project__status">demo</div>
                             </Link>
                             
-                            {/* <Link to='' className='project'>
-                                <img className="project__img" src={img_3d_galley} alt="" />
+                            <a href='https://mgsavto.com/' target="_blank" className='project'>
+                                <img className="project__img" src={img_mgs} alt="" />
                                 <div className='project__content'>
-                                    <div className="project__title"> Проект 1 </div>
-                                    <div className="project__description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus labore recusandae totam voluptatum, quae alias. </div>
+                                    <div className="project__title"> Интернет магазин компании "Магистраль Авто" </div>
+                                    <div className="project__description"> 
+                                        Интернет магазин для ООО "Магистраль Авто". 
+                                        На стеке технологий MERN (MongoDB, Express, React, Node.js). 
+                                        Управление контентом и настройками сайта реализованы через Административную панель.
+                                        (Ещё не вступил в полноценное использование) 
+                                    </div>
                                 </div>
-                                <div className="project__status">demo</div>
-                            </Link>
+                                {/* <div className="project__status">demo</div> */}
+                            </a>
 
-                            <Link to='' className='project'>
+                            {/* <Link to='' className='project'>
                                 <img className="project__img" src={img_3d_galley} alt="" />
                                 <div className='project__content'>
                                     <div className="project__title"> Проект 1 </div>
